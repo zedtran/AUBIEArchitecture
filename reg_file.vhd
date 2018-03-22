6f6ce99ee77974bd1cc3371ae5e472e12ff66f7c
Don <30904702+zedtran@users.noreply.github.com>
2018-03-22 08:38:07 -0500

    Update reg_file.vhd
    Professor comments

--- reg_file.vhd
+++ reg_file.vhd
@@ -14,53 +14,28 @@ entity reg_file is
 		data_out		: out dlx_word;
 	);
 end entity reg_file; -- END ENTITY
+-- 32-bit Datapath -> 8 hex digits instead of 4 (shows as 0x signal in sim)
+-- After write, then apply prop_delay (cannot delay variable assignments)
 
 
 -- BEGIN Define ARCHITECTURE "reg_file" --
 architecture behavior of reg_file is
 		type reg_type is array (0 to 31) of dlx_word;
-		variable registers	: reg_type
+		variable registers	: reg_type;
 begin
 	file_read_write: process(readnotwrite, clock, reg_number, data_in, data_out) is
 	begin 
-	
-		-- ADD CODE HERE
-	
+		-- Start process
+		if (clock = '1') then
+			if (readnotwrite = '0') then
+				-- PERFORM "WRITE"
+				registers(bv_to_integer(reg_number)) := data_in; 
+				-- value needs to be something in array of variables (ignore prop_delay)
+			else
+				-- PERFORM "READ" (readnotwrite = '1')
+				data_out <= registers(bv_to_integer(reg_number)) after prop_delay; 
+				-- copy of variables into the input signal
+			end if;
+		end if;
 	end process file_read_write;
 end architecture behavior; 
-
-
-
-
-
-
---entity cnotGate is 
---	generic(prop_delay: Time:= 10 ns);
---	port(a_in, b_in: in bit; 
---		cnot_out: out bit);
---end entity cnotGate;
---
---architecture behaviour1 of cnotGate is 
---begin
---	cnotProcess: process(a_in, b_in) is
---	
---	begin
---		if a_in = '1' then
---			if b_in = '1' then 
---				-- 1 and 1 = 0 --
---				cnot_out <= '0' after prop_delay;
---			else  
---				-- 1 and 0 = 1 --
---				cnot_out <= '1' after prop_delay;
---		end if;
---		else
---			if b_in = '1' then 
---				-- 0 and 1 = 1 --
---				cnot_out <= '1' after prop_delay;
---			else
---				--0 and 0 = 0 --
---				cnot_out <= '0' after prop_delay;
---			end if;
---		end if;
---	end process cnotProcess;
---end architecture behaviour1;
