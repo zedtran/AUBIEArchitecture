91e361ec072a0dc8c4e37d75c926bd42fc5a1554
zedtran <dzt0021@gmail.com>
2018-03-22 12:41:44 -0700

    Updated and Confirmed ModelSim Compile Success

--- reg_file.vhd
+++ reg_file.vhd
@@ -2,46 +2,69 @@ LIBRARY work;
 USE work.bv_arithmetic.ALL;
 USE work.dlx_types.ALL;
 
--- BEGIN Define ENTITY "reg_file" --
+-------------------------------------------------------------------
+-- 	BEGIN Define ENTITY "reg_file" 				 --
+-- NOTE: We implement a 32-bit Datapath which uses 8 hex digits. --
+-- 		 A 16-bit Datapath would utilize 4 hex digits	 --
+-------------------------------------------------------------------
 entity reg_file is
 	generic(
-		prop_delay		: Time:= 15 ns
+		prop_delay		: Time := 15 ns
 	);
 	-- Signals
 	port(	
 		clock			: in bit;
 		readnotwrite		: in bit;
-		reg_number		: in register_index;		
+		reg_number		: in register_index;
 		data_in 		: in dlx_word;  
 		data_out		: out dlx_word
 	);
-end entity reg_file; -- END ENTITY
--- 32-bit Datapath -> 8 hex digits instead of 4 (shows as 0x signal in sim)
+end entity reg_file; 
 
 
--- BEGIN Define ARCHITECTURE "reg_file" --
+---------------------------------------------
+-- BEGIN Defining ARCHITECTURE "reg_file"  --
+---------------------------------------------
 architecture behavior of reg_file is
-		-- Variables (Storage for our single process)
+		----------------------------------------------------------
+		-- 	Type Define (Act as 'storage' for our process)  --
+		-- reg_type: defines a data structure for		--
+		--	         an array of 32-bit words 		--
+		----------------------------------------------------------
 		type reg_type is array (0 to 31) of dlx_word;
-		variable registers	: reg_type;
 begin
-	-- Process takes input variables from our defined entity above --
-	file_read_write: process(readnotwrite, clock, reg_number, data_in) is
+	reg_file_process: process(readnotwrite, clock, reg_number, data_in) is
+	----------------------------------------------------------------------
+	-- NOTE: Process accepts only input signals from our defined entity --
+	----------------------------------------------------------------------
+		----------------------------------------------------------
+		-- 	Variable (Act as 'storage' for our process)  	--
+		-- registers: implements reg_type and initializes       --
+		--	      registers we use for this process		--
+		----------------------------------------------------------
+		variable registers: reg_type;
 	begin 
 		-- Start process
 		if (clock = '1') then
-			if (readnotwrite = '0') then
-				-- PERFORM "WRITE"
-				registers(bv_to_integer(reg_number)) := data_in; 
-					-- No prop_delay is applied because we don't want to delay variable assignments.
-					-- PROF Comments: "One might argue it takes time to save the value and if you read the value less 
-					-- than 15ns (prop_delay) after writing it you should get the old value, this is hard to model
-					-- so we won't delay "write".
+			if (readnotwrite = '1') then
+				---------------------------------------------------------------
+				-- 	[Performing "READ" Operation (readnotwrite = '1')]   --
+				-- Here, we simply ignore 'data_in' and copy value in 	     --
+				-- registers at index --> reg_number to data_out port signal --
+				---------------------------------------------------------------
+				data_out <= registers(bv_to_integer(reg_number)) after prop_delay;
 			else
-				-- PERFORM "READ" (readnotwrite = '1')
-				data_out <= registers(bv_to_integer(reg_number)) after prop_delay; 
-					-- copy of variables into the input signal
+				------------------------------------------------------
+				-- 	[Performing "WRITE" Operation]		    --
+				-- Value from 'data_in' is copied into registers at --
+				-- register index --> 'reg_number'		    --
+				------------------------------------------------------
+				registers(bv_to_integer(reg_number)) := data_in; 
+				------------------------------------------------------
+				-- NOTE: No prop_delay is applied because we don't  --
+				--	want to delay variable assignments. 	    --
+				------------------------------------------------------
 			end if;
 		end if;
-	end process file_read_write;
+	end process reg_file_process;
 end architecture behavior; 
